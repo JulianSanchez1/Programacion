c073cc903699d1415e5db664ac94ad14b8c17be3
JulianSanchez1 <juliandavidsanchezsanabria@gmail.com>
2019-06-10 11:49:58 -0500

    Add files via upload

---
+++ Funciones.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+/*int main(){//1.el main hace todo
+	float base,altura,area;
+	cout<< "Ingrese la base: ";
+	cin>> base;
+	cout<< "Ingrese la altura: ";
+	cin>>altura;
+	area=(base*altura)/2;
+	cout<<"El area es: "<< area<< endl;
+	return 0;
+}*/
+
+
+/*
+void areat();//prototipo 2 la funcion hace todo
+int main()
+{
+	areat();
+	return 0;
+}
+void areat()//cuerpo funcion
+{
+float b,h,a;
+cout<<"ingrese base: ";
+cin>>b ;
+cout<<"ingrese altura: ";
+cin>>h ;
+a=b*h/2;
+cout<<"El area es:"<< a<<endl;	
+}*/
+
+
+/*void areat(float b,float h);//prototipo 3 El main envia los argumentos
+int main()
+{
+	float base,altura;
+cout<<"ingrese base: ";
+cin>>base ;
+cout<<"ingrese altura: ";
+cin>>altura;
+	areat(base,altura);
+	return 0;
+}
+void areat(float b,float h)//cuerpo funcion
+{
+float a;	
+a=b*h/2;
+cout<<"El area es:"<< a<<endl;	
+}*/
+
+
+/*
+float areat(float b,float h);//prototipo 4 el main envia argumentos , la funcion calcula y retoma un valor 
+int main()
+{
+	float base,altura, area;
+cout<<"ingrese base: ";
+cin>>base ;
+cout<<"ingrese altura: ";
+cin>>altura;
+area=areat(base,altura);
+cout<<"El area es:"<< area<<endl;	
+	return 0;
+}
+float areat(float b,float h)//cuerpo funcion
+{
+float a;	
+a=b*h/2;
+return a;
+}*/
+
