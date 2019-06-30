eff52893ea52ef17b4573b1f9e5ba5921bfc0d82
JulianSanchez1 <juliandavidsanchezsanabria@gmail.com>
2019-06-30 13:53:10 -0500

    Update and rename TallerEstructurasST.cpp to TallerEstructuras.cpp
    Taller estructuras terminado

--- TallerEstructuras.cpp
+++ TallerEstructuras.cpp
@@ -2,13 +2,17 @@
 #include<stdio.h>
 #include<string.h>
 using namespace std;
-main(){
+int main(){
+    char texto[30]="Masculino";
+     char texto1[30]="masculino";
+     int con=0;
     struct datos{
     char nombre[30],apellido[30],sexo[30],direccion[30];
-    int edad,telefono;    
+    int edad,telefono;
     };
     struct datos datosam[5];
-    for (int i=0;i<5;i++){
+    for (int i=0;i<5;i++)
+    {
     	cout<<"ingrese el nombre "<<i+1<<" : ";
     	fflush(stdin);
         gets(datosam[i].nombre);
@@ -17,24 +21,24 @@ main(){
         gets(datosam[i].apellido);
         cout<<"ingrese la edad "<<i+1<<" : ";
         cin>>datosam[i].edad;
-        cout<<"ingrese el sexo "<<i+1<<" : ";
+        cout<<"ingrese el sexo "<<i+1<<" : "<<endl;
+        cout<<"-Masculino \n-Femenino"<< endl;
         fflush(stdin);
 		gets(datosam[i].sexo);
         cout<<"ingrese la direccion "<<i+1<<" : ";
         fflush(stdin);
 		gets(datosam[i].direccion);
+		fflush(stdin);
         cout<<"ingrese la telefono "<<i+1<<" : ";
         cin>>datosam[i].telefono;
         cout<<endl;
     }
-    for (int i=0;i<5;i++){
-    	cout<<"El nombre "<<i+1<<" es : "<< datosam[i].nombre<<endl;
-    	cout<<"El apellido "<<i+1<<" es : "<< datosam[i].apellido<<endl;
-    	cout<<"El edad "<<i+1<<" es : "<< datosam[i].edad<<endl;
-    	cout<<"El sexo "<<i+1<<" es : "<< datosam[i].sexo<<endl;
-    	cout<<"La direccion "<<i+1<<" es : "<< datosam[i].direccion<<endl;
-    	cout<<"El telefono "<<i+1<<" es : "<< datosam[i].telefono<<endl;
-        cout<<endl;
+    for (int i=0;i<5;i++)
+    {
+       if( (strcmp(datosam[i].sexo,texto) == 0 || strcmp(datosam[i].sexo,texto1) == 0) && datosam[i].edad<25 )
+        {
+         con++;
+        }
     }
-  
+    cout<<" El numero de amigos que son hombres y menores de 25 anios es: "<< con;
 }
